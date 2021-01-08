41eafb0f4e488445e0ebef131bd45e59dbd142f9
Juan <juanmanuelynz@gmail.com>
2021-01-08 23:13:37 +0200

    add underline in menu buttons

--- js/main.js
+++ js/main.js
@@ -29,4 +29,51 @@ $(document).ready(function() {
             document.msExitFullscreen();
         }
     }
+    
+        
+/////////////////////////////////////////////////////////////////////
+////////////////////  + link underline effect  //////////////////////
+/////////////////////////////////////////////////////////////////////    
+    
+    function underlineAnim(link, e, index) {
+        let underline = link.querySelector(".underline");
+
+        anime({
+            targets: underline,
+            width: "100%",
+            left: "0%",
+            easing: 'easeOutQuint'
+        });
+    }
+
+    function leaveUnderlineAnim(link, e) {
+        let underline = link.querySelector(".underline");
+
+        anime({
+            targets: underline,
+            width: "0%",
+            left: "0%",
+            easing: 'easeInQuint'
+        });
+    }
+
+    // Get all links
+    let workLinks = document.querySelectorAll(".link");
+    
+    workLinks.forEach(activateLinks);
+        
+    function activateLinks(link, index, value) {
+        
+        link.addEventListener("mouseenter", function(e) {
+            underlineAnim(link, e);
+        });
+
+        link.addEventListener("mouseleave", function(e) {
+            leaveUnderlineAnim(link, e);
+        });
+    }
+/////////////////////////////////////////////////////////////////////
+////////////////////  - link underline effect  //////////////////////
+/////////////////////////////////////////////////////////////////////
+    
 });
