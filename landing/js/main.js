49c863644347b3f4b1db9497e128bc51ccc7e19d
Pepi <jose@samasama.studio>
2021-01-08 18:34:32 +0200

    Add jQuery and test fullscreen.

---
+++ landing/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    var body = $("body");
+    enterFullscreen(body);
+    
+    function enterFullscreen(element) {
+        if(element.requestFullscreen)
+            element.requestFullscreen();
+        else if(element.mozRequestFullScreen)
+            element.mozRequestFullScreen();
+        else if(element.webkitRequestFullscreen)
+            element.webkitRequestFullscreen();
+        else if(element.msRequestFullscreen)
+            element.msRequestFullscreen();
+    }
+    
+    function exitFullscreen() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+});
